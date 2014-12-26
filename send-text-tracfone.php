58ff6864bd5560b5f6218549dcc5be0244dc0c0f
Brandon Smith <vectorcell@gmail.com>
2014-12-26 21:02:01 +0000

    added tracfone number, unverified

---
+++ send-text-tracfone.php
@@ -0,0 +1,28 @@
+<?php
+
+header('Content-Type: application/json');
+
+$number  = $_GET["number"];
+$to      = "$number@mmst5.tracfone.com";
+$subject = "sms";
+$message = wordwrap($_GET["message"], 70, "\r\n");
+$from    = $_GET["from"];
+
+$headers = "From: " . $from . "\r\n" .
+	"Reply-To: " . $from . "\r\n" .
+	"X-Mailer: PHP/" . phpversion();
+
+$sent = mail($to, $subject, $message, $headers);
+
+echo "{\n";
+if ($sent)
+	echo "\t\"sent\":true\n";
+else
+	echo "\t\"sent\":false\n";
+echo "\t\"number\":$number\n";
+echo "\t\"to\":\"$to\"\n";
+echo "\t\"from\":\"$from\"\n";
+echo "\t\"message\":\"$message\"\n";
+
+echo "}\n";
+?>
